e91c91609e436adf4a3b3bcd43e7feaf8f10ab4a
web <webdev2021@gmail.com>
2021-03-15 21:32:40 +0200

    refactoring

--- index.js/index.js
+++ index.js/index.js
@@ -17,30 +17,21 @@ function showSuccess(input) {
 function isValidEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
+};
+function checkRequired(inputArr) { 
+    inputArr.forEach(function(input) {
+        if(input.value.trim() === '') {
+            showError(input, `${getFieldName(input)} is required`);
+        } else {
+            showSuccess(input);
+        }
+    });
+    function getFieldName(input) {
+        return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+    }
 }
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
-    if(username.value === '') {
-        showError(username, 'Username is reqiured');
-    } else {
-        showSuccess(username);
-    }
-    if(email.value === '') {
-        showError(email, 'Email is reqiured');
-    } else if(!isValidEmail(email.value)) {
-        showError(email, 'Email is not valid')
-    } else {
-        showSuccess(email);
-    }
-    if(password.value === '') {
-        showError(password, 'Password is reqiured');
-    } else {
-        showSuccess(password);
-    }
-    if(password2.value === '') {
-        showError(password2, 'Password 2 is reqiured');
-    } else {
-        showSuccess(password2);
-    }
-})
+    checkRequired([username,email,password,password2]);
+});
